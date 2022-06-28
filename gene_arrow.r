dbe43d45d2d322091ff25c4c4421c7de1bcbf318
mschemmel <mschemmel@online.de>
2022-06-28 23:47:32 +0200

    add function documentation

--- gene_arrow.r
+++ gene_arrow.r
@@ -1,7 +1,20 @@
+#' plots an arrow in an defined direction representing a single gene
+
+#' @param x1 x1 coordinate
+#' @param y1 y1 coordinate
+#' @param x2 x2 coordinate
+#' @param y2 y2 coordinate
+#' @param direction Direction of the gene (upstream | downstream)
+#' return grid polygon object
+
+#' @examples
+#' genearrow(1,5,4,6,"downstream")
+
+
 genearrow <- function(x1, y1, x2, y2, direction, ...){
-    arrsize = x2 - x1
+    arrsize <- x2 - x1
     scalefactor <- ifelse(arrsize < 0.4, 0.4, 0.05)
-    arrw = arrsize * scalefactor
+    arrw <- arrsize * scalefactor
     
     if(direction == "downstream"){
          garrow <- grid.polygon(c(x1, x1, x2 - arrw, x2, x2 - arrw),
