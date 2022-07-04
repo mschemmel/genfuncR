5d233609de33a1daa5a38a113ce42ad78a474285
mschemmel <mschemmel@online.de>
2022-07-04 15:51:27 +0200

    cleaner

--- gene_arrow.r
+++ gene_arrow.r
@@ -1,15 +1,3 @@
-#' plots an arrow in an defined direction representing a single gene
-
-#' @param x1 x1 coordinate
-#' @param y1 y1 coordinate
-#' @param x2 x2 coordinate
-#' @param y2 y2 coordinate
-#' @param direction Direction of the gene (upstream | downstream)
-#' return grid polygon object
-
-#' @examples
-#' genearrow(1,5,4,6,"downstream")
-
 # random gff file
 gff <- data.frame(seqname = c("Chr1", "Chr1", "Chr2", "Chr2", "Chr2", "Chr3", "Chr3"),
                     source = rep("Genome",7),
@@ -22,12 +10,21 @@ gff <- data.frame(seqname = c("Chr1", "Chr1", "Chr2", "Chr2", "Chr2", "Chr3", "C
                     attribute = paste0("seq", seq(1,7,1))
                     )
 
-library(grid)
+#' plots an arrow in an defined direction representing a single gene
+
+#' @param x1 x1 coordinate
+#' @param y1 y1 coordinate
+#' @param x2 x2 coordinate
+#' @param y2 y2 coordinate
+#' @param direction Direction of the gene (upstream | downstream)
+#' return grid polygon object
+
+#' @examples
+#' genearrow(1,5,4,6,"downstream")
 
 genearrow <- function(x1, y1, x2, y2, direction, ...){
     arrsize <- x2 - x1
-    #scalefactor <- ifelse(arrsize < unit(0.2,"npc"), unit(0.4,"npc"), unit(0.05, "npc"))
-    arrw <- arrsize * 0.2
+    arrw <- arrsize * 0.2 # TODO: make size dynamic
 
     if(direction == "downstream"){
          grid.polygon(c(x1, x1, x2 - arrw, x2, x2 - arrw),
@@ -40,17 +37,6 @@ genearrow <- function(x1, y1, x2, y2, direction, ...){
     else {
         stop("Invalid 'direction' statement")
     }
-}
-
-text_label <- function(vp_name = NULL, x_, y_, w_, h_, label_txt, angle = 0, gp_ = NULL) {
-    grid::pushViewport(grid::viewport(name = vp_name,
-                                      x = grid::unit(x_, "npc"),
-                                      y = grid::unit(y_, "npc"),
-                                      width = w_,
-                                      height = h_,
-                                      just = c("center")))
-    grid::grid.text(label_txt, gp = gp_, rot = angle)
-    grid::popViewport(1)
 }
 
 geneset <- function(gff_file) {
@@ -82,8 +68,9 @@ geneset <- function(gff_file) {
     genomic_vp_width_x0 <- 0
     genomic_vp_width_x1 <- 1
 
-
+    # helper function
     relative <- function(x, max_x) ((x*genomic_vp_width_x1)/max_x)
+    
     # genomic viewport
     grid::pushViewport(grid::viewport(name = "genomic",
                         x = grid::unit(0.5, "npc"),
@@ -95,27 +82,12 @@ geneset <- function(gff_file) {
     # forward and reverse direction
     grid::grid.segments(x0 = unit(genomic_vp_width_x0, "npc"), y0 = unit(s1_height, "npc"), x1 = unit(genomic_vp_width_x1, "npc"), y1 = unit(s1_height, "npc"))   
     grid::grid.segments(x0 = unit(genomic_vp_width_x0, "npc"), y0 = unit(s2_height, "npc"), x1 = unit(genomic_vp_width_x1, "npc"), y1 = unit(s2_height, "npc"))   
-    grid::grid.xaxis(label = seq(0,max_value, max_value/10), at = seq(0, 1, 0.1))
-
-    # add orientation label
-    #orientations <- c("5'", "3'")
-    #orientation_pos <- rep(c(0, 1), 2)
-    #for (i in c(s1_height, s2_height)) {
-    #    if (i == s2_height) orientations <- rev(orientations)
-    #    for (j in seq(1, 2, 1)) {
-    #        text_label(x_ = orientation_pos[j],
-    #                   y_ = i,
-    #                   w_ = 0.05,
-    #                   h_ = 0.15,
-    #                   label_txt = orientations[j],
-    #                   gp_ = grid::gpar(fontsize = 12, fontface = "bold", col = "black"))
-    #    }
-    #}
+    #grid::grid.xaxis(label = seq(0,max_value, max_value/10), at = seq(0, 1, 0.1))
 
     # add genes
-    for(i in seq(1:nrow(gff))) {
+    for(i in seq(1:nrow(gff_file))) {
         # downstream
-        if(gff$strand[i] == "+") {
+        if(gff_file$strand[i] == "+") {
             genearrow(x1 = unit(relative(gff_file$start[i], max_value), "npc"),
                       y1 = unit(0.7, "npc"),
                       x2 = unit(relative(gff_file$end[i], max_value), "npc"),
@@ -123,7 +95,7 @@ geneset <- function(gff_file) {
         }
 
         # upstream
-        else if (gff$strand[i] == "-") {
+        else if (gff_file$strand[i] == "-") {
             genearrow(x1 = unit(relative(gff_file$start[i], max_value), "npc"),
                       y1 = unit(0.1, "npc"),
                       x2 = unit(relative(gff_file$end[i], max_value), "npc"),
@@ -135,7 +107,6 @@ geneset <- function(gff_file) {
     }
     grid::popViewport(1)
 }
-geneset(gff)
 
 
 
