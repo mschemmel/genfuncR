14bcd80e496c4232ea6a8d845140eea0cff27f4e
mschemmel <mschemmel@online.de>
2022-10-12 00:12:35 +0200

    switch to object oriented style

--- geneset.r
+++ geneset.r
@@ -73,7 +73,7 @@ text_label <- function(vp_name = NULL, x_, y_, w_, h_, label_txt, angle = 0, gp_
 }
 
 #' draw a set of genes based on data.frame or gff file
-#' @param gff_file data.frame of gff file or pure own data.frame
+#' @param gff_file data.frame of gff file
 #' @param forward_color color of genes in forward direction (default = "darkslategray")
 #' @param reverse_color color of genes in reverse direction (default = "darkslategray")
 #' @param transparency alpha value of gene annotation box
@@ -89,6 +89,13 @@ text_label <- function(vp_name = NULL, x_, y_, w_, h_, label_txt, angle = 0, gp_
 #' @examples
 #' geneset(gff)
 
+geneset = setClass("geneset",
+                   slots = list(
+                        gff_file = "ANY",
+                        gene_param = "list",
+                        plot_param = "list"
+                   ))
+
 geneset <- function(gff_file = NULL,
                     forward_color = "darkslategray",
                     reverse_color = "darkslategray",
@@ -102,6 +109,8 @@ geneset <- function(gff_file = NULL,
                     range = NULL,
                     border = FALSE,
                     show_values = FALSE) {
+
+    .Object = new("geneset")
 
     # check if input file is valid
     if (!is.null(gff_file)) {
@@ -118,6 +127,7 @@ geneset <- function(gff_file = NULL,
     # order input by start and end column and filter
     gff_file <- gff_file[with(gff_file, order(gff_file$start, gff_file$end)), ]
     gff_file <- gff_file[gff_file$start > range[1] & gff_file$end < range[2], ]
+    .Object@gff_file = gff_file
 
     if(show_values) print(gff_file[which(gff_file$start > range[1] & gff_file$end < range[2]), ])
 
@@ -135,93 +145,117 @@ geneset <- function(gff_file = NULL,
         stop("Provided axis interval will result in more than 100 labels.")
     }
 
-    # create new device and newpage
-    dev.new(width = 12, height = 6, unit = "in")
-    grid::grid.newpage()
-
-    # outer viewport
-    grid::pushViewport(grid::viewport(name = "outer",
-                                      x = grid::unit(0.5, "npc"),
-                                      y = grid::unit(0.5, "npc"),
-                                      width = 1,
-                                      height = 1))
-
-    # main viewport
-    grid::pushViewport(grid::viewport(name = "main",
-                                      x = grid::unit(0.5, "npc"),
-                                      y = grid::unit(0.5, "npc"),
-                                      width = 0.7,
-                                      height = 0.3,
-                                      just = c("center")))
-
-    # draw border if requested
-    if (border) grid::grid.rect()
-
-    # store some constants
-    min_value <- range[1]
-    max_value <- range[2]
-
+    # overall size of genebox
     gene_box_height <- ifelse(gene_height > 0 & gene_height <= 1,
                               0.2 * gene_height,
                               stop("Height of gene box (gene_height) have to be between 0 and 1."))
-    s2_pos <- 0.2
-    gap <- ifelse(distance >= 0 & distance <= 1,
-                  gene_box_height + ((s2_pos + gene_box_height) * distance),
-                  stop("Distance between forward and reverse strand have to be between 0 and 1."))
+    .Object@gene_param$gene_box_height = gene_box_height
+    
+    # position of forward and reverse strand
+    forward_strand_pos <- 0.2
+    strand_gap <- ifelse(distance >= 0 & distance <= 1,
+                                     gene_box_height + ((forward_strand_pos + gene_box_height) * distance),
+                                     stop("Distance between forward and reverse strand have to be between 0 and 1."))
+    reverse_strand_pos <- forward_strand_pos + strand_gap
 
-    s1_pos <- s2_pos + gap
-    genomic_vp_width_x0 <- 0
-    genomic_vp_width_x1 <- 1
+    .Object@gene_param$forward_strand_pos = forward_strand_pos
+    .Object@gene_param$strand_gap = strand_gap
+    .Object@gene_param$reverse_strand_pos = reverse_strand_pos
+    .Object@gene_param$distance = distance
 
-    # helper function
-    relative <- function(x) (x - min_value) / (max_value - min_value)
+    # define default params
+    .Object@plot_param$min_value = min_value
+    .Object@plot_param$max_value = max_value
+    .Object@gene_param$forward_color = forward_color
+    .Object@gene_param$reverse_color = reverse_color
+    .Object@gene_param$transparency = transparency
+    .Object@gene_param$arrow_type = arrow_type
+    .Object@gene_param$gene_height = gene_height
+    .Object@plot_param$interval = interval
+    .Object@plot_param$show_axis = show_axis
+    .Object@plot_param$axis_label_text = axis_label_text
+    .Object@plot_param$axis_interval = axis_interval
+    .Object@plot_param$border = border
+    .Object@plot_param$show_values = show_values
 
-    # forward direction
-    grid::grid.segments(x0 = grid::unit(genomic_vp_width_x0, "npc"),
-                        y0 = grid::unit(s1_pos, "npc"),
-                        x1 = grid::unit(genomic_vp_width_x1, "npc"),
-                        y1 = grid::unit(s1_pos, "npc"))
-    # reverse direction
-    grid::grid.segments(x0 = grid::unit(genomic_vp_width_x0, "npc"),
-                        y0 = grid::unit(s2_pos, "npc"),
-                        x1 = grid::unit(genomic_vp_width_x1, "npc"),
-                        y1 = grid::unit(s2_pos, "npc"))
-
-
-    # add features of gff
-    for (i in seq_len(nrow(gff_file))) {
-        genearrow(x1 = grid::unit(relative(gff_file$start[i]), "npc"),
-                  y1 = ifelse(gff_file$strand[i] == "+",
-                              grid::unit(s1_pos - (gene_box_height / 2), "npc"),
-                              grid::unit(s2_pos - (gene_box_height / 2), "npc")),
-                  x2 = grid::unit(relative(gff_file$end[i]), "npc"),
-                  y2 = ifelse(gff_file$strand[i] == "+",
-                              grid::unit(s1_pos + (gene_box_height / 2), "npc"),
-                              grid::unit(s2_pos + (gene_box_height / 2), "npc")),
-                  direction = ifelse(gff_file$strand[i] == "+",
-                                     "downstream",
-                                     "upstream"),
-                  gp_ = grid::gpar(fill = ifelse(gff_file$strand[i] == "+",
-                                                 forward_color,
-                                                 reverse_color),
-                                   alpha = transparency),
-                  arr_type = arrow_type)
-    }
-
-    # add axis label
-    if (show_axis) {
-        # set axis label
-        axis_label <- round(seq(min_value, max_value, axis_interval), 0)
-
-        grid::grid.xaxis(label = axis_label,
-                         at = seq(0, 1, 1 / (length(axis_label) - 1)))
-
-        # add axis label text
-        text_label(x_ = 0.5,
-                   y_ = -0.3,
-                   w_ = 0.1,
-                   h_ = 0.2,
-                   label_txt = axis_label_text)
-    }
-    grid::popViewport(1)
+    return(.Object)
 }
+
+setMethod(f = "show",
+          signature = "geneset",
+          definition = function(object) {
+            # helper function
+            relative <- function(x) (x - object@plot_param$min_value) / (object@plot_param$max_value - object@plot_param$min_value)
+            # create new device and newpage
+            dev.new(width = 12, height = 6, unit = "in")
+            grid::grid.newpage()
+
+            # outer viewport
+            grid::pushViewport(grid::viewport(name = "outer",
+                                            x = grid::unit(0.5, "npc"),
+                                            y = grid::unit(0.5, "npc"),
+                                            width = 1,
+                                            height = 1))
+
+            # main viewport
+            grid::pushViewport(grid::viewport(name = "main",
+                                            x = grid::unit(0.5, "npc"),
+                                            y = grid::unit(0.5, "npc"),
+                                            width = 0.7,
+                                            height = 0.3,
+                                            just = c("center")))
+
+            # draw border if requested
+            if (object@plot_param$border) grid::grid.rect()
+
+
+            # forward direction
+            grid::grid.segments(x0 = grid::unit(0, "npc"),
+                                y0 = grid::unit(object@gene_param$reverse_strand_pos, "npc"),
+                                x1 = grid::unit(1, "npc"),
+                                y1 = grid::unit(object@gene_param$reverse_strand_pos, "npc"))
+            # reverse direction
+            grid::grid.segments(x0 = grid::unit(0, "npc"),
+                                y0 = grid::unit(object@gene_param$forward_strand_pos, "npc"),
+                                x1 = grid::unit(1, "npc"),
+                                y1 = grid::unit(object@gene_param$forward_strand_pos, "npc"))
+
+            # add features of gff
+            for (i in seq_len(nrow(object@gff_file))) {
+                genearrow(x1 = grid::unit(relative(object@gff_file$start[i]), "npc"),
+                          y1 = ifelse(object@gff_file$strand[i] == "+",
+                                      grid::unit(object@gene_param$reverse_strand_pos - (object@gene_param$gene_box_height / 2), "npc"),
+                                      grid::unit(object@gene_param$forward_strand_pos - (object@gene_param$gene_box_height / 2), "npc")),
+                          x2 = grid::unit(relative(object@gff_file$end[i]), "npc"),
+                          y2 = ifelse(object@gff_file$strand[i] == "+",
+                                      grid::unit(object@gene_param$reverse_strand_pos + (object@gene_param$gene_box_height / 2), "npc"),
+                                      grid::unit(object@gene_param$forward_strand_pos + (object@gene_param$gene_box_height / 2), "npc")),
+                          direction = ifelse(object@gff_file$strand[i] == "+",
+                                             "downstream",
+                                             "upstream"),
+                          gp_ = grid::gpar(fill = ifelse(object@gff_file$strand[i] == "+",
+                                                         object@gene_param$forward_color,
+                                                         object@gene_param$reverse_color),
+                                          alpha = object@gene_param$transparency),
+                          arr_type = object@gene_param$arrow_type)
+            }
+
+            # add axis label
+            if (object@plot_param$show_axis) {
+                # set axis label
+                axis_label <- round(seq(object@plot_param$min_value, object@plot_param$max_value, object@plot_param$axis_interval), 0)
+
+                grid::grid.xaxis(label = axis_label,
+                                 at = seq(0, 1, 1 / (length(axis_label) - 1)))
+
+                # add axis label text
+                text_label(x_ = 0.5,
+                           y_ = -0.3,
+                           w_ = 0.1,
+                           h_ = 0.2,
+                           label_txt = object@plot_param$axis_label_text)
+            }
+            grid::popViewport(1)
+            })
+
+geneset(gff, range = c(0,3000), axis_interval = 500)
