a203a3903744df366e55949ca355abb7fe96585d
mschemmel <mschemmel@online.de>
2023-03-16 22:05:09 +0100

    add border color capability

--- dna2img.r
+++ dna2img.r
@@ -10,14 +10,14 @@
 #' @examples
 #' textLabel(x_ = 1, y_ = 3, w_ = 1, h_ = 1, label_txt = "Test", angle = 45)
 
-textLabel <- function(vp_name = NULL, x_, y_, w_, h_, label_txt = NULL, angle = 0, gp_ = NULL, bg) {
+textLabel <- function(vp_name = NULL, x_, y_, w_, h_, label_txt = NULL, angle = 0, gp_ = NULL, bg, borderCol) {
     grid::pushViewport(grid::viewport(name = vp_name,
                                       x = grid::unit(x_, "npc"),
                                       y = grid::unit(y_, "npc"),
                                       width = w_,
                                       height = h_,
                                       just = c("bottom")))
-    grid::grid.rect(gp = grid::gpar(fill = bg))
+    grid::grid.rect(gp = grid::gpar(fill = bg, col = borderCol))
     grid::grid.text(label_txt, gp = gp_, rot = angle)
     grid::popViewport(1)
 }
@@ -57,7 +57,8 @@ dna2img <- setClass("dna2img",
                     slots = list(sequence = "character",
                                  base_colors = "list",
                                  letter_per_line = "numeric",
-                                 background = "character"),
+                                 background = "character",
+                                 border = "character"),
                     prototype = list(base_colors = list(A = "#9EE362",
                                                         T = "#00C0D0",
                                                         G = "#FFD403",
@@ -65,16 +66,19 @@ dna2img <- setClass("dna2img",
                                                         U = "#d83131",
                                                         N = "gray60"),
                                      letter_per_line = 20,
-                                     background = "gray80"))
+                                     background = "gray80",
+                                     border = "white"))
 
 dna2img <- function(sequence,
                     base_colors,
                     letter_per_line = 20,
-                    background = "gray80") {
+                    background = "gray80",
+                    border = "white") {
     .Object <- new("dna2img")
     .Object@sequence <- unlist(strsplit(sequence, split = ""))
     .Object@letter_per_line <- letter_per_line
     .Object@background <- background
+    .Object@border <- border
     return(.Object)
 }
 
@@ -100,6 +104,7 @@ setMethod(f = "show",
                   w_ = 0.05,
                   h_ = 0.05,
                   bg = object@background,
+                  borderCol = object@border,
                   gp_ = grid::gpar(fontsize = 14,
                                    fontface = "bold",
                                    col = object@base_colors[[object@sequence[x]]]))
