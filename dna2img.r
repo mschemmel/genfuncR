913c7c17d4e1770fe4c24ded8ded3cc46adccd96
mschemmel <mschemmel@online.de>
2023-03-19 22:22:46 +0100

    make box font size reactive

--- dna2img.r
+++ dna2img.r
@@ -1,25 +1,38 @@
 #' puts a text label on a specific position
-#' @param vp_name Name of the viewport
+#' @param vp_name name of the viewport
 #' @param x_ x coordinate
 #' @param y_ y coordinate
 #' @param w_ w coordinate
 #' @param h_ h coordinate
-#' @param label_txt Text to be printed on that position
-#' @param angle Angle of text label
-#' @param gp_ Grid parameter like font, style, color, ...
+#' @param label_txt text to be printed on that position
+#' @param angle angle of text label
+#' @param gp_ grid parameter like font, style, color, ...
+#' @param cex fontsize of nucleotide
 #' @examples
 #' textLabel(x_ = 1, y_ = 3, w_ = 1, h_ = 1, label_txt = "Test", angle = 45)
 
-textLabel <- function(vp_name = NULL, x_, y_, w_, h_, label_txt = NULL, angle = 0, gp_ = NULL, bg, borderCol) {
+textLabel <- function(vp_name = NULL, x_, y_, w_, h_, label_txt = NULL, angle = 0, gp_ = NULL, cex = 0.4, bg, borderCol) {
     grid::pushViewport(grid::viewport(name = vp_name,
                                       x = grid::unit(x_, "npc"),
                                       y = grid::unit(y_, "npc"),
                                       width = w_,
                                       height = h_,
-                                      just = c("bottom")))
+                                      just = c("bottom"),
+                                      gp = grid::gpar(fontsize = cex)))
     grid::grid.rect(gp = grid::gpar(fill = bg, col = borderCol))
     grid::grid.text(label_txt, gp = gp_, rot = angle)
     grid::popViewport(1)
+}
+
+#' get width and heigth of text label of given fontsize
+#' @param fontsize fontsize of nucleotide
+#' @examples
+#' nuclDims("A")
+nuclDims <- function(fontsize) {
+    nucleotides <- c("A", "T", "C", "G", "N")
+    nucl_width <- max(unlist(lapply(nucleotides, function(x) strwidth(x, cex = fontsize, units = "inches"))))
+    nucl_height <- max(unlist(lapply(nucleotides, function(x) strheight(x, cex = fontsize, units = "inches"))))
+    return(c(nucl_width, nucl_height))
 }
 
 #' calculate x coordinates of nucleotides
@@ -56,6 +69,7 @@ yCoords <- function(seq_, letter) {
 dna2img <- setClass("dna2img",
                     slots = list(sequence = "character",
                                  base_colors = "list",
+                                 cex = "numeric",
                                  letter = "numeric",
                                  background = "character",
                                  border = "character"),
@@ -65,18 +79,21 @@ dna2img <- setClass("dna2img",
                                                         "C" = "#FF9356",
                                                         "U" = "#d83131",
                                                         "N" = "gray60"),
+                                     cex = 0.4,
                                      letter = 20,
                                      background = "gray80",
                                      border = "white"))
 
 dna2img <- function(sequence,
                     base_colors,
+                    cex = 0.4,
                     letter = 20,
                     background = "gray80",
                     border = "white") {
     .Object <- new("dna2img")
     .Object@sequence <- unlist(strsplit(sequence, split = ""))
     .Object@letter <- letter
+    .Object@cex <- cex
     .Object@background <- background
     .Object@border <- border
     return(.Object)
@@ -94,20 +111,20 @@ setMethod(f = "show",
 
     # draw all nucleotides
     grid::pushViewport(grid::viewport(x = 0.5, y = 0.5, width = 0.95, height = 0.95))
-
-    lapply(seq_along(object@sequence), function(x){
+    lapply(seq_along(object@sequence), function(x) {
         xpos <- coordx[x]
         ypos <- coordy[x]
         textLabel(label_txt = object@sequence[x],
                   x_ = xpos,
                   y_ = ypos,
-                  w_ = (0.95 / object@letter) / 2,
-                  h_ = (0.95 / object@letter) / 2,
+                  w_ = nuclDims(fontsize = object@cex)[1],#(0.95 / object@letter) / 2,
+                  h_ = nuclDims(fontsize = object@cex)[2],#(0.95 / object@letter) / 2,
                   bg = object@background,
+                  cex = object@cex,
                   borderCol = object@border,
-                  gp_ = grid::gpar(fontsize = 14,
+                  gp_ = grid::gpar(fontsize = 24,
                                    fontface = "bold",
                                    col = object@base_colors[[object@sequence[x]]]))
-        })
+    })
     grid::popViewport(1)
 })
