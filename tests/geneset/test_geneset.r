2bdb133536dc6c27cf6469e83cf10e0ef48e6353
mschemmel <mschemmel@online.de>
2023-06-30 23:44:04 +0200

    more tests

--- tests/geneset/test_geneset.r
+++ tests/geneset/test_geneset.r
@@ -1,6 +1,24 @@
 test.helpers <- function() {
   expect_equal(last(c(1:3)), 3)
-  expect_equal(first(c(1:3)), 2)
+  expect_equal(first(c(1:3)), 1)
   expect_equal(dropLast(c(1:3)), c(1,2))
+  expect_true(inRange(3,0,10))
+  expect_true(inRange(3,3,10))
+  expect_false(inRange(3,4,10))
+  expect_true(inRange(10,3,10))
+  expect_false(inRange(11,3,10))
+
 }
 test.helpers()
+
+test.layout <- function() {
+  expect_equal(getAnnoYScale(numeric(0)), c(0, 0.5, 1))
+  expect_equal(getAnnoYScale(2), pretty(c(0, 2)))
+  expect_equal(getAnnoYScale(c(1:10), c(2:5)), pretty(c(2:5)))
+
+  expect_equal(getAnnoYBreaks(numeric(0)), c(0, 0.5, 1))
+  expect_equal(getAnnoYBreaks(c(1:3)), c(0, 0.5, 1))
+  expect_equal(getAnnoYBreaks(c(1:2)), c(0, 0.5, 1))
+}
+
+test.layout()
